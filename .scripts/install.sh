93ee324360f78240465c22e625523a9071c9e829
Lucas Taniguti <lucas.biotec07@gmail.com>
2023-12-16 15:07:23 -0300

    🚀 Install script

---
+++ .scripts/install.sh
@@ -0,0 +1,73 @@
+set -e
+
+
+print_message() {
+  local message="$1"
+  local severity="$2"
+  local red='\033[0;31m'
+  local green='\033[0;32m'
+  local yellow='\033[1;33m'
+  local nc='\033[0m'
+
+  case "${severity}" in
+    "info" ) echo -e "${nc}${message}${nc}";;
+    "ok" ) echo -e "${green}${message}${nc}";;
+    "error" ) echo -e "${red}${message}${nc}";;
+    "warn" ) echo -e "${yellow}${message}${nc}";;
+    *) echo -e "${message}";; # Default case if severity is not recognized
+  esac
+}
+
+# # check if release.tar.gz exists
+# if [ -f release.tar.gz ]; then
+#     print_message "release.tar.gz already exists" "warn"
+#     exit 1
+# fi
+
+# # check if wget is installed
+# if ! [ -x "$(command -v wget)" ]; then
+#     print_message "wget is not installed" "warn"
+#     exit 1
+# fi
+
+# # check if wdl-hub directory exists
+# if [ -d wdl-hub ]; then
+#     print_message "wdl-hub directory already exists. Remove it to install a new release" "warn"
+#     exit 1
+# fi
+
+# # witch github release to download? default to v0.1.3 (ex: v0.1.3)
+# echo "Which release do you want to download? (default: v0.1.3)"
+# read release
+# # if empty then default to v0.1.3
+# if [ -z "$release" ]; then
+#     release="v0.1.3"
+# fi
+
+# # print the release
+# echo "Downloading release $release"
+# wget https://github.com/lmtani/wdl-hub/releases/download/$release/release.tar.gz
+# tar -xf release.tar.gz
+# rm release.tar.gz
+
+
+print_message "Done!" "ok"
+
+print_message "To import tasks from the WDL Hub, add lines like these into you WDL file:" "info"
+
+print_message "import \"wdl-hub/tasks/wget.wdl\"" "ok"
+
+print_message "And then you can call \"wget.Wget\" in your workflow" "info"
+
+print_message "Once you are done, you can build you release with pumbaa build. It will create the .wdl and .zip files" "info"
+
+print_message "Do you want to add the wdl-hub directory to your .gitignore file? (y/n)" "info"
+read answer
+if [ "$answer" == "y" ]; then
+    print_message "Adding wdl-hub to .gitignore" "ok"
+    echo "wdl-hub" >> .gitignore
+fi
+
+print_message "Suggestion:" "ok"
+print_message "- If you need a highly custom task that is not in the WDL Hub, you can create a new task in a \"tasks\" directory and then import it in your WDL file." "info"
+print_message "- If you want to contribute to the WDL Hub with a generic task, you can fork the repository and then create a pull request." "info"
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# wdl-hub
+# WDL-hub
 
-A centralized repository for organizing, storing, and sharing WDL tasks and workflows.
+A centralized repository for organizing, storing, and sharing WDL tasks and workflows. Currently it supports only WDL 1.0.
 
 > [!NOTE]
 > This repository is a work in progress. I use it to centralize my WDL tasks and workflows so that I can easily reuse them in my projects.
@@ -17,16 +17,43 @@ The project is organized into the following directories:
     * `<workflow-name>`: Contains a WDL workflow and its associated files.
         * `tests`: Contains tests for the WDL workflow.
 
-## Releases
+## Tests
 
-We use Pumbaa to build our pipeline releases. It packs all dependencies of a WDL workflow into a single zip file. This zip file can be used to run the workflow on Cromwell.
+Here we just test that the WDL tasks and workflows can be run without errors. We don't test the correctness of the results.
 
-To build a release, run the following command:
+## How to use it
+
+1. Download the release .tar.gz file from the releases page.
+1. Extract in the directory you will develop your WDL workflow.
+1. Import the tasks and workflows you want to use in your WDL workflow.
+
+### Importing tasks
+
+To import a task, add the following line to the top of your WDL file:
+
+```wdl
+import "wdl-hub/tasks/<module-name>.wdl" as a_name
+```
+
+Then you can use the task in your WDL workflow:
+
+```wdl
+workflow my_workflow {
+    call a_name.task_name { input: ... }
+}
+```
+
+### Building a workflow
+
+We use [Pumbaa]() to build our workflows. It packs all dependencies of a WDL workflow into a single zip file. This zip file can be used to run the workflow on Cromwell.
+
+To build a workflow, run the following command:
 
 ```bash
+
 # Install pumbaa
 curl https://raw.githubusercontent.com/lmtani/pumbaa/main/assets/install.sh | bash
 
-# Build a release
-pumbaa build --wdl pipelines/benchmark_genome_assemblies/BenchmarkGenomeAssemblies.wdl --out releases/
+# Build a workflow
+pumbaa build --wdl MyWorkflow.wdl --out releases/
 ```
